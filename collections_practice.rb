3e07048a2af22ac39ecc7d9dd9d9f869b58a8f44
Sid He <xhe518@gmail.com>
2018-07-13 21:08:58 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,14 @@
 def sort_array_asc(integers)
   integers.sort
 end
+
+def sort_array_desc(integers)
+  integers.sort do |a, b|
+  if a == b
+    0
+  elsif a < b
+    1
+  elsif a > b
+    -1
+  end
+end
