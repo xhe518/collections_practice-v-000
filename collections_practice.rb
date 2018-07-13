ccc0d2913f5b9d4199a8ab50e982a2820328e8d2
Sid He <xhe518@gmail.com>
2018-07-13 22:18:20 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -43,7 +43,7 @@ def kesha_maker(array)
 end
 
 def find_a(array)
-  array.select! {|string| string.start_with? == "a"}
+  array.select! {|string| string.start_with?("a")}
 end
 
 def sum_array(array)
