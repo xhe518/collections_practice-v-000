a79b25b09a7160dbaafa9e35a16fa29dccaa7df1
Sid He <xhe518@gmail.com>
2018-07-13 21:19:51 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,15 +1,27 @@
-def sort_array_asc(integers)
-  integers.sort
+def sort_array_asc(array)
+  array.sort
 end
 
-def sort_array_desc(integers)
-  integers.sort do |a, b|
+def sort_array_desc(array)
+  array.sort do |a, b|
     if a == b
       0
     elsif a < b
       1
     elsif a > b
       -1
+    end
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
     end
   end
 end
