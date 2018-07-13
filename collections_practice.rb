9647299d0c881b9162ad043214e3a3da4aaf5868
Sid He <xhe518@gmail.com>
2018-07-13 22:40:02 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,63 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a < b
+      1
+    elsif a > b
+      -1
+    end
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1] #parallel_assignment
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |string|
+    string[2] = "$"
+  end
+  array
+end
+
+def find_a(array)
+  array.select! {|string| string.start_with?("a")}
+end
+
+def sum_array(array)
+  sum = 0
+  array.each {|a| sum += a}
+  sum
+end
+
+def add_s(array)
+  array.each_with_index.collect do |string, index|
+    if index != 1
+      string = string + "s"
+    else
+      string
+    end
+  end
+end
