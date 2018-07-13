39e061c61d9050ba65481146da71ca9c4b019e0f
Sid He <xhe518@gmail.com>
2018-07-13 21:45:12 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,6 +27,17 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  array[1], array[2] = array[2], array[1]
+  array[1], array[2] = array[2], array[1] #parallel_assignment
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |string|
+    string[2] = "$"
+  end
   array
 end
